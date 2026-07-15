bd7954b505772f0d7b928ffa2f997a41cd7c2b32
Arpit Soni <151952369+Arpitsoni89@users.noreply.github.com>
2026-07-15 23:50:46 +0530

    Update package version from 0.1.0 to 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="greet-package-updated",
-    version="0.1.0",
+    version="0.0.1",
     description="A simple greeting package",
     author="Arpit Soni",
     author_email="your-email@example.com",

3d5bda88a0e755b235e5012074ed7a13e3484bd2
Arpit Soni <151952369+Arpitsoni89@users.noreply.github.com>
2026-07-16 13:40:53 +0530

    Bump package version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="greet-package-updated",
-    version="0.3.0",
+    version="0.4.0",
     author="Arpit Soni",
     description="A simple greeting package",
     packages=find_packages(),
